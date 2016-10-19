2d884defe347bf7066d0acb22b0bdd51a2305a9a
Marlon Taborda <marlondtaborda@gmail.com>
2016-10-18 20:48:20 -0500

    fixing that verguerete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,15 +7,9 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(post_params)
-<<<<<<< 9a03a6a98cb81e90ebd485b869c5eb93965cefd9
     if task.save
       flash[:notice] = "Task created!"
     end
-=======
-      if task.save
-          flash[:notice] = "Task created!"
-      end
->>>>>>> Creatre controller for tasks, and the base index interface
   end
 
   private 
