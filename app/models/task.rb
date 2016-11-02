854bba34190d96b86c5890b3262acf79a26c3e1e
victorhenao <victorhenao@gmail.com>
2016-11-01 19:59:11 -0500

    Add relation from task to user

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,10 @@
 class Task < ApplicationRecord
     belongs_to :category
     belongs_to :project
+    belongs_to :user
+
+    # def for_user_in_this_week(current_user)
+    #   today = Date.
+    #   Task.where()
+    # end
 end
