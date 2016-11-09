fcdaf6e4bb92f93f0abb0b6e4c6850045833cc4b
Marlon Taborda <marlondtaborda@gmail.com>
2016-11-08 20:32:18 -0500

    Finish billable time case

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :billable_hours
+
+  def billable_hours
+    @total_billable_hours = Task.billable_hours(current_user, true)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,28 +1,28 @@
 class Task < ApplicationRecord
+  # Constants
   BILLABLE_TIME = 42
   BILLABLE_VACATIONS_TIME = 42
 
+  # Relationships
   belongs_to :category
   belongs_to :project
   belongs_to :user
 
-    def self.for_user_in_this_week(current_user)
-      where(user_id: current_user.id).
-      get_current_week_tasks
-    end
+  # Validations
+  validates :description, presence: true
+  validates_length_of :description, minimun: 50, maximum: 500 
 
-    def self.get_current_week_tasks
-      where("created_at >= ? AND created_at <= ?", Date.current.at_beginning_of_week, Date.current.at_end_of_week)
-    end
+  # Scopes
+  scope :current_user, ->(user) {  where(user_id: user.id) }
+  scope :current_weekly, -> {  where("created_at >= ? AND created_at <= ?", Date.current.at_beginning_of_week, Date.current.at_end_of_week) }
+  scope :weekly_current_task, ->(user) { current_user(user).current_weekly }
 
-    validates :description, presence: true
-    validates_length_of :description, minimun: 50, maximum: 500 
+  # Class methods
+  def self.billable_hours(user, billable)
+    current_user(user).current_weekly.is_billable?(billable).sum(:hours)
+  end
 
-    def get_billable_hours
-        
-    end
-
-    def get_unbillable_hours
-        
-    end
+  def self.is_billable? value
+    where(billable: value)
+  end
 end
