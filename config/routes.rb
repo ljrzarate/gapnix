edabfd35be88543a3414e60a05add546085e50e0
victorhenao <victorhenao@gmail.com>
2016-11-22 21:02:26 -0500

    Delete an specified task

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   resources :sessions, only: :index
   get "/auth/:provider/callback" => 'sessions#create'
   resource :dashboard, only: :show
-  resources :tasks, only: [:index,:create]
-
+  resources :tasks, only: [:index, :create, :destroy]
   resources :users do
     resources :projects, controller: "users/projects" do
       resources :categories, controller: "users/categories"
