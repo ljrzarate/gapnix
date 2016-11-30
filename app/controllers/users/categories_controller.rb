d8d94d943ccc3a98dc9de0c1f8986420597ba085
Felipe Correa <Felipe Correa>
2016-11-29 21:29:47 -0500

    Categories admin started

--- app/controllers/users/categories_controller.rb
+++ app/controllers/users/categories_controller.rb
@@ -1,6 +1,52 @@
 class Users::CategoriesController < ApplicationController
 
     def index
+        @categories = current_user.projects.find(params[:project_id]).categories
     end
 
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = current_user.projects.new(category_params)
+        
+        if @category.valid?
+          @category.save
+          flash.now[:notice] = "Category created!"
+          redirect_to user_projects_path
+        else
+          flash[:alert] = @category.errors.full_messages.to_sentence
+          render :new
+        end
+    end
+
+    def edit
+        @category = current_user.projects.find(params[:project_id]).categories.find(params[:id])
+    end
+
+    def update
+        @category = current_user.projects.find(params[:project_id]).categories.find(params[:id])
+        @category.attributes = category_params
+        
+        if @category.valid?
+          @category.save
+          flash.now[:notice] = "Category updated!"
+          redirect_to user_projects_path
+        else
+          flash[:alert] = @category.errors.full_messages.to_sentence
+          render :edit
+        end
+    end
+
+    def destroy
+        current_user.projects.find(params[:project_id]).categories.find(params[:id]).destroy
+        flash[:success] = "Category deleted"
+        redirect_to user_projects_path
+    end
+
+    private 
+        def category_params
+            params.require(:category).permit(:name, :description)
+        end
 end
