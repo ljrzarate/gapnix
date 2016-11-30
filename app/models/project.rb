a84297eb8cdb8f082a91993388d0a441c9adb6ec
Luis Rodriguez <ljrzarate@gmail.com>
2016-11-29 19:32:56 -0500

    Fixing issue so not need to restart server each time something is change in models/controllers

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
-    has_many :tasks
-    has_many :categories
-    belongs_to :user
+  has_many :tasks
+  has_many :categories
+  belongs_to :user
 
-    validates :name, presence: true
+  validates :name, presence: true
 end
