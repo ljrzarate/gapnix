6eaee8d1ff64030be549500f6e531cb84ceff0e7
yperez <yperez.gap@gmail.com>
2016-11-29 20:42:15 -0500

    redirecting the user if is logged to dashboard or to the login screen if is not logged.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
+
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :user && action_name == 'new'
+      "login"
+    else
+      "application"
+    end
+  end
+
 end
