5a7c58588fbd836948d36d0d19b4c19023b519c9
Luis Rodriguez <ljrzarate@gmail.com>
2017-01-24 18:58:27 -0500

    adding basic scrapper

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'omniauth-google-oauth2'
 gem 'will_paginate-bootstrap'
 
 gem 'pg'
+gem 'mechanize'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,6 @@ Bundler.require(*Rails.groups)
 module Gapnix
   class Application < Rails::Application
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.autoload_paths << "#{Rails.root}/lib"
   end
 end
