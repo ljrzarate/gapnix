8592ec4c81715705626083f4f2fa98e014223456
Luis Rodriguez <ljrzarate@gmail.com>
2017-01-31 22:56:07 -0500

    Refactoring of the scrapper - Is missing a way to how get the week day to be assigned

--- lib/journyx/scrapper.rb
+++ lib/journyx/scrapper.rb
@@ -10,30 +10,52 @@ require 'watir'
 
 module Journyx
   class Scrapper
-    JOURNYX_URL = "https://growthaccel.apps.journyx.com/jtcgi/wte.pyc"
-    attr_reader :username, :password, :project, :page, :category
+    JOURNYX_URL = "https://growthaccel.apps.journyx.com/jtcgi/wte.pyc?TE=1&"
+
+    attr_reader :username, :password, :project, :category, :browser, :description
+
     def initialize(params = {})
-      @username = params.fetch(:username)
-      @username = params.fetch(:password)
-      @username = params.fetch(:project)
-      @username = params.fetch(:category)
+      @username    = params.fetch(:username)
+      @password    = params.fetch(:password)
+      @project     = params.fetch(:project)
+      @category    = params.fetch(:category)
+      @description = params.fetch(:description)
+      @hours       = params.fetch(:hours)
+      init_browser
     end
 
     def post_data
-      browser = Watir::Browser.new :phantomjs
-      browser.window.maximize
-      browser.goto "https://growthaccel.apps.journyx.com/jtcgi/wte.pyc?TE=1&"
+      login
+      set_description
+      set_project
+      sleep 3
+      set_category
+      set_hours
+      submit_form
+    end
 
-      #Login
+    private
+    def init_browser
+      @browser = Watir::Browser.new :phantomjs
+      browser.window.maximize
+      browser.goto(JOURNYX_URL)
+    end
+
+    def login
       browser.text_field(name: "wtusername").set(username)
       browser.text_field(name: "wtpassword").set(password)
 
       browser.form(name: "a").submit
+    end
 
+    def set_description
       left_table = browser.tbody(id: "idMainTableLeftbody")
       description_row = left_table.tr(class: "rowtype_New").tds.last
-      description_row.text_fields.first.set("Hola 2")
+      description_row.text_fields.first.set(description)
+    end
 
+    def set_project
+      left_table = browser.tbody(id: "idMainTableLeftbody")
       project_row = left_table.tr(class: "rowtype_New").tds.first
 
       project_row.select_lists.first.options.each do |option|
@@ -41,22 +63,27 @@ module Journyx
           option.click
         end
       end
-      sleep 3
+    end
+
+    def set_category
+      left_table = browser.tbody(id: "idMainTableLeftbody")
       category_row = left_table.tr(class: "rowtype_New").tds[1]
       category_row.select_lists.first.options.each do |option|
         if option.text.downcase.strip == category.downcase.strip
           option.click
         end
       end
+    end
 
+    def set_hours
       right_table = browser.tbody(id: "idMainTableRightbody")
       hour_row = right_table.tr(class: "rowtype_New").tds[2]
-      hour_row.text_fields.first.set("9:30")
+      hour_row.text_fields.first.set(hours)
+    end
 
+    def submit_form
       save_button = browser.input(id: "enterAction")
-
       save_button.click
-
       browser.close
     end
   end
